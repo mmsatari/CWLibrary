3fff95f6bebf291cc42abffa4f12a70eb043ff17
Mostafa Satari <m@msatari.ir>
2014-05-21 19:23:40 +0430

    added convertion to morse sysmbols

--- CWLibrary/TextToMorse.cs
+++ CWLibrary/TextToMorse.cs
@@ -7,13 +7,6 @@ namespace CWLibrary
 {
     public class TextToMorse
     {
-        // Character speed in WPM
-        public int CharacterSpeed { get; set; }
-        // Overall speed in WPM (must be <= character speed)
-        public int WordSpeed { get; set; }
-        // Tone frequency
-        public double Frequency { get; set; }
-
         public TextToMorse(int charSpeed, int wordSpeed, double frequency)
         {
             CharacterSpeed = charSpeed;
@@ -21,21 +14,51 @@ namespace CWLibrary
             Frequency = frequency;
         }
 
-        public TextToMorse(int charSpeed, int wordSpeed) : this(charSpeed, wordSpeed, 600.0) { }
-        public TextToMorse(int wpm) : this(wpm, wpm) { }
-        public TextToMorse() : this(20) { }
+        public TextToMorse(int charSpeed, int wordSpeed)
+            : this(charSpeed, wordSpeed, 600.0)
+        {
+        }
+
+        public TextToMorse(int wpm)
+            : this(wpm, wpm)
+        {
+        }
+
+        public TextToMorse()
+            : this(20)
+        {
+        }
+
+        // Character speed in WPM
+        public int CharacterSpeed { get; set; }
+
+        // Overall speed in WPM (must be <= character speed)
+        public int WordSpeed { get; set; }
+
+        // Tone frequency
+        public double Frequency { get; set; }
+
+        // Returns a byte array in the Wave file format containing the given
+        // text in morse code
+        public byte[] ConvertToMorse(string text)
+        {
+            DataChunk data = GetText(text.ToLower());
+            FormatChunk formatChunk = new FormatChunk();
+            HeaderChunk headerChunk = new HeaderChunk(formatChunk, data);
+            return headerChunk.ToBytes();
+        }
 
         // Return given number of seconds of sine wave
         private short[] GetWave(double seconds)
         {
             short[] waveArray;
-            int samples = (int)(11025 * seconds);
-            
+            int samples = (int) (11025*seconds);
+
             waveArray = new short[samples];
 
             for (int i = 0; i < samples; i++)
             {
-                waveArray[i] = Convert.ToInt16(32760 * Math.Sin(i * 2 * Math.PI * Frequency / 11025));
+                waveArray[i] = Convert.ToInt16(32760*Math.Sin(i*2*Math.PI*Frequency/11025));
             }
 
             return waveArray;
@@ -49,7 +72,7 @@ namespace CWLibrary
         private short[] GetSilence(double seconds)
         {
             short[] waveArray;
-            int samples = (int)(11025 * seconds);
+            int samples = (int) (11025*seconds);
 
             waveArray = new short[samples];
 
@@ -59,19 +82,19 @@ namespace CWLibrary
         // Dot -- 1 unit long
         private short[] GetDot()
         {
-            return GetWave(1.2 / CharacterSpeed);
+            return GetWave(1.2/CharacterSpeed);
         }
 
         // Dash -- 3 units long
         private short[] GetDash()
         {
-            return GetWave(3.6 / CharacterSpeed);
+            return GetWave(3.6/CharacterSpeed);
         }
 
         // Inter-element space -- 1 unit long
         private short[] GetInterEltSpace()
         {
-            return GetSilence(1.2 / CharacterSpeed);
+            return GetSilence(1.2/CharacterSpeed);
         }
 
         // Space between letters -- nominally 3 units, but adjusted for
@@ -80,8 +103,8 @@ namespace CWLibrary
         // http://www.arrl.org/files/file/Technology/x9004008.pdf
         private short[] GetInterCharSpace()
         {
-            double delay = (60.0 / WordSpeed) - (32.0 / CharacterSpeed);
-            double spaceLength = 3 * delay / 19;
+            double delay = (60.0/WordSpeed) - (32.0/CharacterSpeed);
+            double spaceLength = 3*delay/19;
             return GetSilence(spaceLength);
         }
 
@@ -90,8 +113,8 @@ namespace CWLibrary
         // speed.
         private short[] GetInterWordSpace()
         {
-            double delay = (60.0 / WordSpeed) - (32.0 / CharacterSpeed);
-            double spaceLength = 7 * delay / 19;
+            double delay = (60.0/WordSpeed) - (32.0/CharacterSpeed);
+            double spaceLength = 7*delay/19;
             return GetSilence(spaceLength);
         }
 
@@ -103,7 +126,7 @@ namespace CWLibrary
             short[] dash = GetDash();
             List<short> morseChar = new List<short>();
 
-            string morseSymbol = Characters.Symbols[character];
+            string morseSymbol = Characters.InternationalSymbols[character];
             for (int i = 0; i < morseSymbol.Length; i++)
             {
                 if (i > 0)
@@ -168,16 +191,6 @@ namespace CWLibrary
             DataChunk dataChunk = new DataChunk(data.ToArray<short>());
 
             return dataChunk;
-        }
-
-        // Returns a byte array in the Wave file format containing the given
-        // text in morse code
-        public byte[] ConvertToMorse(string text)
-        {
-            DataChunk data = GetText(text.ToLower());
-            FormatChunk formatChunk = new FormatChunk();
-            HeaderChunk headerChunk = new HeaderChunk(formatChunk, data);
-            return headerChunk.ToBytes();
         }
     }
 }
---
+++ CWLibrary/TextToMorseSysbols.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CWLibrary
+{
+    public class TextToMorseSymbols
+    {
+        public TextToMorseSymbols(int charSpeed, int wordSpeed, double frequency)
+        {
+            CharacterSpeed = charSpeed;
+            WordSpeed = wordSpeed;
+            Frequency = frequency;
+        }
+
+        public TextToMorseSymbols(int charSpeed, int wordSpeed)
+            : this(charSpeed, wordSpeed, 600.0)
+        {
+        }
+
+        public TextToMorseSymbols(int wpm)
+            : this(wpm, wpm)
+        {
+        }
+
+        public TextToMorseSymbols()
+            : this(20)
+        {
+        }
+
+        // Character speed in WPM
+        public int CharacterSpeed { get; set; }
+
+        // Overall speed in WPM (must be <= character speed)
+        public int WordSpeed { get; set; }
+
+        // Tone frequency
+        public double Frequency { get; set; }
+
+        // Returns a byte array in the Wave file format containing the given
+        // text in morse code
+        public string ConvertToMorse(string text)
+        {
+            string symbols = GetText(text.ToLower());
+            return symbols;
+        }
+
+        // Return given number of seconds of sine wave
+        private short[] GetWave(double seconds)
+        {
+            short[] waveArray;
+            int samples = (int) (11025*seconds);
+
+            waveArray = new short[samples];
+
+            for (int i = 0; i < samples; i++)
+            {
+                waveArray[i] = Convert.ToInt16(32760*Math.Sin(i*2*Math.PI*Frequency/11025));
+            }
+
+            return waveArray;
+        }
+
+        // Return given number of seconds of flatline. This could also be
+        // achieved with slnt chunks inside a wavl chunk, but the resulting
+        // file might not be universally readable. If saving space is that
+        // important, it would be better to compress the output as mp3 or ogg
+        // anyway.
+        private short[] GetSilence(double seconds)
+        {
+            short[] waveArray;
+            int samples = (int) (11025*seconds);
+
+            waveArray = new short[samples];
+
+            return waveArray;
+        }
+
+        // Return a single character as a waveform
+        private string GetCharacter(string character)
+        {
+            return Characters.InternationalSymbols[character];
+        }
+
+        // Return a word as a waveform
+        private string GetWord(string word)
+        {
+            string data = "";
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (i > 0)
+                    data += " ";
+                if (word[i] == '<')
+                {
+                    // Prosign
+                    int end = word.IndexOf('>', i);
+                    if (end < 0)
+                        throw new ArgumentException();
+                    data += GetCharacter(word.Substring(i, end + 1 - i));
+                    i = end;
+                }
+                else
+                {
+                    data += GetCharacter(word[i].ToString());
+                }
+            }
+
+            return data;
+        }
+
+        // Return a string (lower case text only, unrecognized characters
+        // throw an exception -- see Characters.cs for the list of recognized
+        // characters) as a waveform wrapped in a DataChunk, ready to by added
+        // to a wave file.
+        private string GetText(string text)
+        {
+            string data = "";
+
+            string[] words = text.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (i > 0)
+                    data += " ";
+                data += GetWord(words[i]);
+            }
+
+            return data;
+        }
+    }
+}
